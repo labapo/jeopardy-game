251d643369decdba5a1c6764312f57f648618195
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-12-05 14:36:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+Project Title: 
+Jeopardy Coding Game
+
+Motivation: 
+This Jeopardy game will be a trivia-like, two player game. This game will have Javascript review questions
+
+Planning: 
+Two Classes - Player 
+Class for cards? 
+
+Game Specifics-
+- Make a grid of 4 categories 5 questions each - goal is to make at least 60 questions. When in testing phase, just use 5 questions to test. Save the 60 questions for the weekend so maybe you can put it in the array or something. 
+- Each player will have a turn
+- Each player will pick a category and number to answer the question. IF they get it right the scoreboard will update. Stretch, if they get it wrong, the opposing team member will pick the category and score of answer
+- When choosing an answer, they will have a multiple choice question
+- there will be a timer to answer with the song
+- final jeopardy will have 
+
+Controlled by buttons- 
+- button to choose category and score amount
+- button for the answer choice
+- Reset button to reset the whole game and go to 0, and reset the round 
+
+Win State - person with the highest score after 2 rounds and final jeopardy
+
+stretch DOUBLE JEOPARDY
+
+Where I see issues- 
+- how to develop different rounds
+- timer and making sure they answer within time (set timeout )
+
+
+Build Status
+
+
+
+
+Code Style
+
+
+
+
+Screenshots
+
+
+
+
+Tech/Framework Used
+example: Built with Electron
+
+
+Features
+
+
+
+Installation 
+
+
+
+API Reference
+
+
+
+Tests
+
+
+
+
+How to use
+
+
+Credits
+Give proper credits 
---
+++ jeopardy.js
@@ -0,0 +1,48 @@
+/*Pseudocode
+- start game
+1. Click "Start" in the directions box to start game. Start will dissapear in the directions box after game starts. 
+2. Enter names for player 1 and player 2
+    - create a start game function
+
+- start player 1 turn
+1. Directions for player 1 to choose a category and amount
+2. Pull up question screen
+3. Will have 25 seconds to answer the question
+4. If player 1 answers the question correctly by clicking on the correct answer, they will receive the points
+5. If player 1 answers the question incorrectly, they will have those points deducted
+6. Screen goes back to the score board with player 1's scores updated
+
+- start player 2 turn
+1. Directions for player 2 to choose a category and amount
+2. Pull up question screen
+3. Will have 25 seconds to answer the question. 
+4. If player 2 answers the question correctly by clicking on the correct answer, they will receive the points
+5. If player 2 answers the question incorrectly, they will have those points deducted
+6. Screen goes back to the score board with player 2's scores updated
+
+- End round
+When all questions have been answered, go into final jeopardy where they have to type out the question. 
+
+- final jeopardy
+Will go to the questions screen. Will have category and prompt user to make their wager. 
+Have one question and they have to type it out with no misspellings. Make sure you have the who is, what is, what are. 
+
+- End game
+Winner is the person who has the most points at the end will win! 
+
+- Buttons
+1. There's a reset game button that will make the score 0, delete player's names, and reset all questions
+2. There's a reset round that will reset the game to the beginning of the round. Will use if I think there should be two rounds
+3. There's a start game button that will be in the player's directions box. Start will disspear after the start button is pressed
+
+- Two classes
+1. player turn
+2. generate questions
+
+stretches: 
+1. Have two rounds
+2. Daily Double
+3. If you get question wrong, the other player choose question and amount
+4. Add an interface to submit questions to go into the array
+5. Maybe have a page where I can see all the questions? 
+*/

f311bcb8a7e516232d25aec4ff6463d04bbbb632
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-12-06 19:34:51 -0700

    styled table and added directions section

--- jeopardy.js
+++ jeopardy.js
@@ -3,7 +3,6 @@
 1. Click "Start" in the directions box to start game. Start will dissapear in the directions box after game starts. 
 2. Enter names for player 1 and player 2
     - create a start game function
-
 - start player 1 turn
 1. Directions for player 1 to choose a category and amount
 2. Pull up question screen
@@ -11,7 +10,6 @@
 4. If player 1 answers the question correctly by clicking on the correct answer, they will receive the points
 5. If player 1 answers the question incorrectly, they will have those points deducted
 6. Screen goes back to the score board with player 1's scores updated
-
 - start player 2 turn
 1. Directions for player 2 to choose a category and amount
 2. Pull up question screen
@@ -19,22 +17,17 @@
 4. If player 2 answers the question correctly by clicking on the correct answer, they will receive the points
 5. If player 2 answers the question incorrectly, they will have those points deducted
 6. Screen goes back to the score board with player 2's scores updated
-
 - End round
 When all questions have been answered, go into final jeopardy where they have to type out the question. 
-
 - final jeopardy
 Will go to the questions screen. Will have category and prompt user to make their wager. 
 Have one question and they have to type it out with no misspellings. Make sure you have the who is, what is, what are. 
-
 - End game
 Winner is the person who has the most points at the end will win! 
-
 - Buttons
 1. There's a reset game button that will make the score 0, delete player's names, and reset all questions
 2. There's a reset round that will reset the game to the beginning of the round. Will use if I think there should be two rounds
 3. There's a start game button that will be in the player's directions box. Start will disspear after the start button is pressed
-
 - Two classes
 1. player turn
 2. generate questions
@@ -42,7 +35,6 @@ Winner is the person who has the most points at the end will win!
     answer
     value
     level
-
 stretches: 
 1. Have two rounds
 2. Daily Double
@@ -275,7 +267,8 @@ function addCategory (category) {
         category.questions.forEach(question => {
         const card = document.createElement('div')
         card.classList.add('card')
-        categoryBoard.append(card)
+        const questionsGrid = document.getElementById('questions-grid')
+        questionsGrid.append(card)
         if (question.level===1) { 
          card.innerHTML = 100
         }
@@ -295,3 +288,7 @@ function addCategory (category) {
    })
 }
 jeopardyQuestions.forEach(category => addCategory(category))
+
+function startFunction () {
+document.getElementById('start-game').style.color = "red";
+}
